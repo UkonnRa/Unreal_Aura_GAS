908d1aed6cfbc4dbeaf6cdfa8a208739e9f1290d
Ukonn Ra <ukonnra@outlook.com>
2023-12-28 22:02:10 +0800

    feat: Add Player and Enemy characters

--- Source/Aura/Private/Characters/AuraCharacterBase.cpp
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -1,10 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AuraCharacterBase.h"
+#include "Characters/AuraCharacterBase.h"
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
 }
